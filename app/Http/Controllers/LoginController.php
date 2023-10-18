20efc9c0a62ad1e47e464762f3772606d3a13038
AlternateFern <ayatabg2@gmail.com>
2023-10-18 10:28:02 +0700

    early

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class LoginController extends Controller
+{
+    function index(){
+        return view('login');
+    }
+
+    function login(Request $request){
+        $dataLogin = $request->only("username", "password");
+        if(Auth::attempt($dataLogin)){
+            return redirect('/home');
+        }else{
+            return redirect('/login');
+        }
+    }
+
+    function logout(){
+        // Session::flush();
+        Auth::logout();
+
+        return redirect('/login');
+    }
+}
---
+++ resources/views/isi_pengaduan.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Isi Pengaduan</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/index.css">
+</head>
+
+<body>
+  @include('layouts.navbar')
+  <div class="container mt-5">
+    <div class="mb-3">
+      <form action="isi_pengaduan" method="POST" enctype="multipart/form-data">
+        @method("POST")
+        @csrf
+
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Isi Laporan Anda</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="isi_laporan"></textarea>
+          @error('isi_laporan')
+            <div> {{ $message }}</div>
+          @enderror
+        </div>
+        <label for="exampleFormControlTextarea1" class="form-label">Bukti berbentuk File Gambar atau Video</label>
+        <div class="input-group mb-3">
+          <input type="file" class="form-control" id="inputGroupFile02" name="foto">
+        </div>
+        <button type="submit" class="btn btn-success ml-auto">Kirim</button>
+        <a href="{{ url('home') }}" class="btn btn-warning ml-auto" style="margin-left: 7px;">Kembali</a>
+      </form>
+    </div>
+</body>
+
+</html>
